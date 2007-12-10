f0b3e7117104e483c4f7cb01a70dda8d1fe026ef
Jeremy Huddleston Sequoia <jeremyhu@freedesktop.org>
2007-12-10 07:49:16 +0000

    Script I use to setup my repos.
    git-svn-id: https://svn.macosforge.org/repository/xquartz/trunk@2 9052f27e-8171-453d-b9bb-b0e8541e09c8

---
+++ setup-repos.sh
@@ -0,0 +1,24 @@
+#GIT_BASE="git://anongit.freedesktop.org/git/xorg/util/lndir"
+GIT_BASE="ssh://git.freedesktop.org/git"
+
+svn checkout --username jeremyhu@freedesktop.org http://svn.macosforge.org/repository/xquartz
+mv xquartz svn-xquartz
+
+mkdir src
+cd src
+git-clone ${GIT_BASE}/xorg/app/xauth.git
+git-clone ${GIT_BASE}/xorg/app/xinit.git
+git-clone ${GIT_BASE}/xorg/util/lndir.git
+git-clone ${GIT_BASE}/xorg/lib/libX11.git
+
+git-clone ${GIT_BASE}/xorg/xserver.git
+cd xserver
+git-branch --track xorg-server-1.2-apple origin/xorg-server-1.2-apple
+git-branch --track xorg-server-1.4-apple origin/xorg-server-1.4-apple
+git-branch --track server-1.3-branch origin/server-1.3-branch
+git-branch --track server-1.4-branch origin/server-1.4-branch
+git-checkout -f xorg-server-1.2-apple
+ln -s ../../svn-xquartz/trunk/compile-xserver.sh compile.sh
+
+cd ../..
+ln -s src/xserver
