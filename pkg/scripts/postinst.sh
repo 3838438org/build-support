f346582ac4b1c368d43caf25abeb55daabf4e417
Jeremy Huddleston Sequoia <jeremyhu@freedesktop.org>
2007-12-11 20:40:42 +0000

    Added mkfontdir/mkfontscale.
    git-svn-id: https://svn.macosforge.org/repository/xquartz/trunk@22 9052f27e-8171-453d-b9bb-b0e8541e09c8

--- pkg/scripts/postinst.sh
+++ pkg/scripts/postinst.sh
@@ -1,7 +1,15 @@
 #!/bin/sh
+# Load the fontconfig launchd.plist
 /bin/launchctl load -w /System/Library/LaunchAgents/org.x.fontconfig.plist
-/usr/X11/bin/fc-cache
 
+# Update font info
+/usr/X11/bin/fc-cache
+for d in {,/System}/Library/Fonts ; do
+	/usr/X11/bin/mkfontdir ${d}
+	/usr/X11/bin/mkfontscale ${d}
+done
+
+# Kill off old X11.app
 if [[ -d /usr/X11/X11.app ]] ; then
         rm -rf /usr/X11/X11.app
 fi
