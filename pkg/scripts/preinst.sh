0279252108753c3527a02bba796578d2721b620e
Jeremy Huddleston Sequoia <jeremyhu@freedesktop.org>
2008-04-11 16:35:11 +0000

    More 2.2.0 updates
    git-svn-id: https://svn.macosforge.org/repository/xquartz/trunk@47 9052f27e-8171-453d-b9bb-b0e8541e09c8

--- pkg/scripts/preinst.sh
+++ pkg/scripts/preinst.sh
@@ -4,6 +4,14 @@ if [[ -d /usr/X11/X11.app ]] ; then
         rm -rf /usr/X11/X11.app
 fi
 
+# Remove these, so the new compressed man pages are used
+if [[ -f /usr/share/man/man1/quartz-wm.1 ]] ; then
+	rm /usr/share/man/man1/quartz-wm.1
+fi
+if [[ -d /usr/X11/man && ! -h /usr/X11/man ]] ; then
+	rm -rf /usr/X11/man
+fi
+
 # This plist is the old name (and the future name for X11.app)
 if [ -f /System/Library/LaunchAgents/org.x.X11.plist ] ; then
 	/bin/launchctl unload -w /System/Library/LaunchAgents/org.x.X11.plist
