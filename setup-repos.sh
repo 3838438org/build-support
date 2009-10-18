57de043d50df5010f32a4aecce2bc20bb3d53f8c
Jeremy Huddleston Sequoia <jeremyhu@freedesktop.org>
2009-10-18 23:44:07 +0000

    Updated checkouts
    git-svn-id: https://svn.macosforge.org/repository/xquartz/trunk@374 9052f27e-8171-453d-b9bb-b0e8541e09c8

--- setup-repos.sh
+++ setup-repos.sh
@@ -1,36 +1,33 @@
 #GIT_BASE="git://anongit.freedesktop.org/git"
 GIT_BASE="ssh://git.freedesktop.org/git"
 
-svn checkout --username jeremyhu@freedesktop.org http://svn.macosforge.org/repository/xquartz
-mv xquartz svn-xquartz
+#svn checkout --username jeremyhu@freedesktop.org http://svn.macosforge.org/repository/xquartz
+#mv xquartz svn-xquartz
 
 mkdir src
 cd src
-curl -LO http://superb-west.dl.sourceforge.net/sourceforge/mesa3d/MesaLib-6.5.2.tar.bz2
-tar -xjvf MesaLib-6.5.2.tar.bz2
-curl -LO http://superb-west.dl.sourceforge.net/sourceforge/mesa3d/MesaLib-7.0.4.tar.bz2
-tar -xjvf MesaLib-7.0.4.tar.bz2
-curl -LO ftp://invisible-island.net/xterm/xterm-229.tgz
-tar -xjzf xterm-229.tgz
-git-clone ${GIT_BASE}/fontconfig
-git-clone ${GIT_BASE}/xorg/app/xauth.git
-git-clone ${GIT_BASE}/xorg/app/xinit.git
-git-clone ${GIT_BASE}/xorg/util/lndir.git
-git-clone ${GIT_BASE}/xorg/lib/libX11.git
-git-clone ${GIT_BASE}/xorg/lib/libXfont
+#curl -LO http://superb-west.dl.sourceforge.net/sourceforge/mesa3d/MesaLib-6.5.2.tar.bz2
+#tar -xjvf MesaLib-6.5.2.tar.bz2
+#curl -LO http://superb-west.dl.sourceforge.net/sourceforge/mesa3d/MesaLib-7.0.4.tar.bz2
+#tar -xjvf MesaLib-7.0.4.tar.bz2
+#curl -LO ftp://invisible-island.net/xterm/xterm-229.tgz
+#tar -xjzf xterm-229.tgz
+#git-clone ${GIT_BASE}/fontconfig
+#git-clone ${GIT_BASE}/xorg/app/xauth.git
+#git-clone ${GIT_BASE}/xorg/app/xinit.git
+#git-clone ${GIT_BASE}/xorg/util/lndir.git
+#git-clone ${GIT_BASE}/xorg/lib/libX11.git
+#git-clone ${GIT_BASE}/xorg/lib/libXfont
 
-git-clone ${GIT_BASE}/xorg/xserver.git
-mv xserver xserver-1.4
-cd xserver-1.4
-git-branch --track xorg-server-1.4-apple origin/xorg-server-1.4-apple
-git-branch --track server-1.4-branch origin/server-1.4-branch
-git-checkout -f xorg-server-1.4-apple
-ln -s ../../svn-xquartz/trunk/compile-xserver.sh compile.sh
+git clone ${GIT_BASE}/xorg/xserver.git
+mv xserver xserver-master
 
-cd ..
-git-clone ${GIT_BASE}/xorg/xserver.git
-mv xserver xserver-1.5
-cd xserver-1.5
-git-branch --track xorg-server-1.5-apple origin/xorg-server-1.5-apple
-git-branch --track server-1.5-branch origin/server-1.5-branch
-git-checkout -f xorg-server-1.5-apple
+for branch in 1.4 1.5 1.6 1.7 ; do
+	git clone --reference xserver-master ${GIT_BASE}/xorg/xserver.git
+	mv xserver xserver-${branch}
+	cd xserver-${branch}
+	git branch --track xorg-server-${branch}-apple origin/xorg-server-${branch}-apple
+	git checkout -f xorg-server-${branch}-apple
+	ln -s ../compile-xserver.sh compile.sh
+	cd ..
+done
