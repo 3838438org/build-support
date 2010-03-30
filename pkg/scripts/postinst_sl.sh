4ea8c7a14ae39b1fafbbdeb06d6e71aa614e4b46
Jeremy Huddleston Sequoia <jeremyhu@freedesktop.org>
2010-03-30 01:27:05 +0000

    Remove paths.d files from beta versions
    git-svn-id: https://svn.macosforge.org/repository/xquartz/trunk@435 9052f27e-8171-453d-b9bb-b0e8541e09c8

--- pkg/scripts/postinst_sl.sh
+++ pkg/scripts/postinst_sl.sh
@@ -2,4 +2,7 @@
 # Cache system fonts
 /opt/X11/bin/font_cache --force --system
 
+[[ -f /etc/paths.d/XQuartz ]] && rm /etc/paths.d/XQuartz 
+[[ -f /etc/manpaths.d/XQuartz ]] && rm /etc/manpaths.d/XQuartz 
+
 exit 0
