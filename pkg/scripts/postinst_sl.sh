b35fe49b8f0fed21839c1467c35a398fd4d3a6fd
Jeremy Huddleston Sequoia <jeremyhu@freedesktop.org>
2010-07-10 17:49:13 +0000

    Reload the LaunchDaemon after install
    git-svn-id: https://svn.macosforge.org/repository/xquartz/trunk@464 9052f27e-8171-453d-b9bb-b0e8541e09c8

--- pkg/scripts/postinst_sl.sh
+++ pkg/scripts/postinst_sl.sh
@@ -2,6 +2,10 @@
 # Cache system fonts
 /opt/X11/bin/font_cache --force --system
 
+# Load the privileged_startx daemon
+/bin/launchctl unload -w /Library/LaunchDaemons/org.macosforge.xquartz.privileged_startx.plist
+/bin/launchctl load -w /Library/LaunchDaemons/org.macosforge.xquartz.privileged_startx.plist
+
 [[ -f /etc/paths.d/XQuartz ]] && rm /etc/paths.d/XQuartz 
 [[ -f /etc/manpaths.d/XQuartz ]] && rm /etc/manpaths.d/XQuartz 
 
