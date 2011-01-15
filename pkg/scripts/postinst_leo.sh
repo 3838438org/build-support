c0049db3a013bba6ea9310d6fca1cae603b93742
Jeremy Huddleston Sequoia <jeremyhu@freedesktop.org>
2011-01-15 21:53:57 +0000

    Remove old libpng SDKs during postinst
    git-svn-id: https://svn.macosforge.org/repository/xquartz/trunk@502 9052f27e-8171-453d-b9bb-b0e8541e09c8

--- pkg/scripts/postinst_leo.sh
+++ pkg/scripts/postinst_leo.sh
@@ -1,13 +1,13 @@
 #!/bin/sh
-# Cache system fonts
-/usr/X11/bin/font_cache --force --system
+
+[[ -d /usr/X11/include/libpng14 ]] && rm -rf /usr/X11/include/libpng14
+[[ -f /usr/X11/bin/libpng14-config ]] && rm /usr/X11/bin/libpng14-config
 
 # Load the privileged_startx daemon
 /bin/launchctl unload -w /System/Library/LaunchDaemons/org.x.privileged_startx.plist >& /dev/null
 /bin/launchctl load -w /System/Library/LaunchDaemons/org.x.privileged_startx.plist
 
-#if [ -d /usr/X11/share/X11/xkb ] ; then
-#	rm -rf /usr/X11/share/X11/xkb
-#fi
+# Cache system fonts
+/usr/X11/bin/font_cache --force --system
 
 exit 0
--- pkg/scripts/postinst_sl.sh
+++ pkg/scripts/postinst_sl.sh
@@ -1,12 +1,19 @@
 #!/bin/sh
-# Cache system fonts
-/opt/X11/bin/font_cache --force --system
+
+[[ -f /etc/paths.d/XQuartz ]] && rm /etc/paths.d/XQuartz 
+[[ -f /etc/manpaths.d/XQuartz ]] && rm /etc/manpaths.d/XQuartz 
+
+[[ -d /opt/X11/include/libpng12 ]] && rm -rf /opt/X11/include/libpng12
+[[ -f /opt/X11/bin/libpng12-config ]] && rm /opt/X11/bin/libpng12-config
+
+[[ -d /opt/X11/include/libpng14 ]] && rm -rf /opt/X11/include/libpng14
+[[ -f /opt/X11/bin/libpng14-config ]] && rm /opt/X11/bin/libpng14-config
 
 # Load the privileged_startx daemon
 /bin/launchctl unload -w /Library/LaunchDaemons/org.macosforge.xquartz.privileged_startx.plist
 /bin/launchctl load -w /Library/LaunchDaemons/org.macosforge.xquartz.privileged_startx.plist
 
-[[ -f /etc/paths.d/XQuartz ]] && rm /etc/paths.d/XQuartz 
-[[ -f /etc/manpaths.d/XQuartz ]] && rm /etc/manpaths.d/XQuartz 
+# Cache system fonts
+/opt/X11/bin/font_cache --force --system
 
 exit 0
